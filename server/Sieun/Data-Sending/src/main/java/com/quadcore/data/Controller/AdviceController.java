c8e9f71e3a0cdd19e84446dae50090adc73804af
Sieun Sim <tlatldms@naver.com>
2020-02-13 22:40:20 +0900

    added AdviceController for error handling in data-sending server

---
+++ server/Sieun/Data-Sending/src/main/java/com/quadcore/data/Controller/AdviceController.java
@@ -0,0 +1,44 @@
+package com.quadcore.data.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class AdviceController {
+    private Logger logger = LoggerFactory.getLogger(ApplicationRunner.class);
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public Map<String, Object> duplicateEx(Exception e) {
+        logger.warn("DataIntegrityViolationException" + e.getClass());
+        Map<String, Object> map = new HashMap<>();
+        map.put("errorCode", 53);
+        return map;
+    }
+
+
+    @ExceptionHandler({
+            IllegalArgumentException.class, MissingServletRequestParameterException.class})
+    public Map<String, Object> paramsEx(Exception e) {
+        logger.warn("params ex: "+ e);
+        Map<String, Object> map = new HashMap<>();
+        map.put("errorCode", 51);
+        return map;
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public Map<String, Object> nullEx(Exception e) {
+        logger.warn("null ex" + e.getClass());
+        Map<String, Object> map = new HashMap<>();
+        map.put("errorCode", 61);
+        return map;
+    }
+
+}
