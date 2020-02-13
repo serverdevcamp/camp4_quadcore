8d8efe3686854636a8cead29239ea46530e9f78c
Sieun Sim <tlatldms@naver.com>
2020-02-13 14:21:24 +0900

    fixed data path

--- server/Sieun/Data-Sending/src/main/java/com/quadcore/data/Controller/CasanController.java
+++ server/Sieun/Data-Sending/src/main/java/com/quadcore/data/Controller/CasanController.java
@@ -39,14 +39,14 @@ public class CasanController {
     @Autowired
     RedisTemplate<String, Object> redisTemplate;
 
-    @GetMapping(path = "/test")
+    @GetMapping(path = "/data/test")
     public @ResponseBody List<Casan> test() {
         List<Casan> products = new ArrayList<>();
         casanRepository.findAll().forEach(products::add); //fun with Java 8
         return products;
     }
 
-    @PostMapping(path="/add")
+    @PostMapping(path="/data/add")
     public void savee(@RequestBody Map<String, Object> m) {
         System.out.println(redisTemplate);
         Casan casan = new Casan();
