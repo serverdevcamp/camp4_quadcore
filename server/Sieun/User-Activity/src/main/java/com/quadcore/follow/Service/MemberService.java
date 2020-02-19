8ca4c985bd07156d33ee2f9d4bf35b8011c00cec
Sieun Sim <tlatldms@naver.com>
2020-02-19 17:19:32 +0900

    to merge

--- server/Sieun/User-Activity/src/main/java/com/quadcore/follow/Service/MemberService.java
+++ server/Sieun/User-Activity/src/main/java/com/quadcore/follow/Service/MemberService.java
@@ -3,6 +3,7 @@ package com.quadcore.follow.Service;
 import com.quadcore.follow.Domain.Member;
 import com.quadcore.follow.Repository.MemberRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,6 +12,8 @@ import java.util.ArrayList;
 @Service
 @AllArgsConstructor
 public class MemberService {
+
+    @Autowired
     private MemberRepository repository;
 
     public Member findMemberByUsername(String username) {
@@ -19,7 +22,8 @@ public class MemberService {
 
     public void updateTweetMembers (String member, String tweetMember) {
         Member mem = repository.findMemberByUsername(member);
-        //System.out.println("in service: " + mem);
+
+        System.out.println("updateTweetmemver of : " + mem);
         ArrayList<String> fl = mem.getTweetMembers();
         fl.add(tweetMember);
         mem.setTweetMembers(fl);
