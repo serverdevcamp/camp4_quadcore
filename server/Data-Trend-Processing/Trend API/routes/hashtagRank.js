83d25ab1ce5cefa0f14aea13405ba3f69ba5f2b2
MoonYeeun <aams82@hanmail.net>
2020-02-25 01:02:58 +0900

    change routes name

---
+++ server/Data-Trend-Processing/Trend API/routes/hashtagRank.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+const redis = require('../module/redis');
+const moment = require('moment');
+
+
+// 실시간 해시태그 트렌드 보여주기 
+router.get('/', async (req, res) => {
+    let currentTime = moment().format("YYYY/MM/DD/HH/mm");
+    try {
+        result = await redis.get(currentTime, 1);
+        console.log(result);
+        if(!result) {
+            res.status(200).send({
+                state : 'fail',
+                errorCode : 40,
+                message : 'there is no data'
+              }
+            )
+        } else {
+            res.status(200).send({
+                state : 'success',
+                errorCode : 10,
+                message : result
+              }
+            )
+        }
+    } catch(err) {
+        res.status(200).send({
+            state : 'fail',
+            errorCode : 52,
+            message : err
+          }
+        )
+    }
+});
+
+module.exports = router;
