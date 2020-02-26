ec00832069ee6d7045566dbab4ff510cc23faa1a
shinychan95 <shinychan95@gmail.com>
2020-02-26 17:07:47 +0900

    change folder name

--- server/DataTrendProcessing/hashtag_Trend.py
+++ server/DataTrendProcessing/hashtag_Trend.py
@@ -1,8 +1,9 @@
 import pyspark
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
-import json, time, datetime, redis
+import json, time,redis
 from itertools import chain
+from datetime import datetime
 
 myRedis = redis.Redis(host='10.240.14.39', port=6379, password='12341234', db=1)
 
